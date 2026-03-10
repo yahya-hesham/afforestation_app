ab0da56e3506a359969aa455a2506125e4e95108
yahya.hesham.saleh@gmail.com <yahya.hesham.saleh@gmail.com>
2026-03-10 18:03:24 +0200

    the first main commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# afforestation_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/afforestation_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.afforestation_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
