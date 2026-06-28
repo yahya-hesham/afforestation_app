76322f9ae25b4fdf984c9c1d11bad62e65fd032d
yahya.hesham.saleh@gmail.com <yahya.hesham.saleh@gmail.com>
2026-06-28 05:38:24 +0300

    add inter font

--- lib/core/constants/app_fonts.dart
+++ lib/core/constants/app_fonts.dart
@@ -1,3 +1,7 @@
+
 abstract class AppFonts {
- 
+
 }
+
+
+
--- lib/core/styles/text_styles.dart
+++ lib/core/styles/text_styles.dart
@@ -1,7 +1,15 @@
 import 'package:bookia/core/styles/colors.dart';
 import 'package:flutter/material.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 abstract class TextStyles {
 
-  
+
+ // Your exact styling from the CSS panel: Inter, 20px, Bold (700)
+
+final TextStyle loginHeaderStyle = GoogleFonts.inter(
+  fontSize: 20.0,                
+  fontWeight: FontWeight.w700, 
+  color: AppColors.primary   
+
+);
 }
