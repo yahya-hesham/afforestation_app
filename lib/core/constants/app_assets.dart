f25022ed28ab8f289311a6c83ca1b8d8dce096e9
yahya.hesham.saleh@gmail.com <yahya.hesham.saleh@gmail.com>
2026-06-28 04:32:28 +0300

    adding some Icons and Assets

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,5 @@
+class AppAssets {
+  static const String treesvg = 'assets/icons/tree.svg';
+  static const String plantCropssvg = 'assets/icons/plantCrops.svg';
+  static const String bellsvg = 'assets/icons/bell.svg';
+}
