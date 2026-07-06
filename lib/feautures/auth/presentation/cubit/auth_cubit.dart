ae8059cd424fb7548bfa39da9a7cc045b79eb7df
yahya.hesham.saleh@gmail.com <yahya.hesham.saleh@gmail.com>
2026-07-06 22:01:41 +0300

    finissh cubit for register screen

--- lib/feautures/auth/presentation/cubit/auth_cubit.dart
+++ lib/feautures/auth/presentation/cubit/auth_cubit.dart
@@ -1,5 +1,5 @@
-  import 'package:bookia/feautures/auth/presentation/cubit/auth_state.dart';
-  import 'package:flutter_bloc/flutter_bloc.dart';
+  import 'package:afforestation_app/feautures/auth/presentation/cubit/auth_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
   class AuthCubit extends Cubit<AuthState> {
     AuthCubit() : super(AuthInitialState());
