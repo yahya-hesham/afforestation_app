8a53cab81cff168aa6d88f4829640a091c6811aa
Abdoae35 <Abdelrahman3535.elsheref@gmail.com>
2026-07-06 17:16:15 +0300

    FixTheBottomNavigationBar

---
+++ lib/feautures/search/presentation/page/search.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Search extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Search Page')),
+      body: const Center(child: Text('This is the search page.')),
+    );
+  }
+}
