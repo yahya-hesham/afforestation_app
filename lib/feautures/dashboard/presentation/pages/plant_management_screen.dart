617a81cc8f2d87bb1a22ee109cd0400faf97587c
Abdoae35 <Abdelrahman3535.elsheref@gmail.com>
2026-07-08 20:25:16 +0300

    SetupLocalization

--- lib/feautures/dashboard/presentation/pages/plant_management_screen.dart
+++ lib/feautures/dashboard/presentation/pages/plant_management_screen.dart
@@ -1,4 +1,4 @@
-import 'package:afforestation_app/feautures/dashboard/presentation/models/plant_model.dart';
+import 'package:afforestation_app/feautures/dashboard/data/repository/models/plant_model.dart';
 import 'package:afforestation_app/feautures/dashboard/presentation/widgets/category_selection_card.dart';
 import 'package:afforestation_app/feautures/dashboard/presentation/widgets/plant_app_bar.dart';
 import 'package:afforestation_app/feautures/dashboard/presentation/widgets/plant_bottom_nav_bar.dart';
